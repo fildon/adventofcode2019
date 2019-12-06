dad711f0f7c4c3db303b27b6874ea2058e3780f9
Rupert McKay <maxfmckay@gmail.com>
2019-12-06 06:44:06 +0000

    06a SOLVED. Previous commit was actually fine, just needed to strip the input

--- 06a/06a.py
+++ 06a/06a.py
@@ -1,7 +1,7 @@
 class OrbitalTree:
     def __init__(self, file_name):
         file = open(file_name, 'r')
-        self.orbits = list(map(lambda line: line.split(')'), file.read().split('\n')))
+        self.orbits = list(map(lambda line: line.split(')'), file.read().strip().split('\n')))
         self.scores = dict({'COM': 0})
 
     def get_orbital_score(self):
@@ -11,7 +11,8 @@ class OrbitalTree:
                 score += self.scores[self.orbits[0][0]] + 1
                 self.scores[self.orbits[0][1]] = self.scores[self.orbits[0][0]] + 1
                 self.orbits = self.orbits[1:]
-            self.orbits = self.orbits[1:] + self.orbits[:1]
+            else:
+                self.orbits = self.orbits[1:] + self.orbits[:1]
         return score
 
 
