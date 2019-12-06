dbb85051ea3d8ddb5f3d6afbbf8a6059b19666a2
Rupert McKay <maxfmckay@gmail.com>
2019-12-06 06:38:10 +0000

    06a solves the test input, but is too inefficient for the real (large!) input

---
+++ 06a/06a.py
@@ -0,0 +1,18 @@
+class OrbitalTree:
+    def __init__(self, file_name):
+        file = open(file_name, 'r')
+        self.orbits = list(map(lambda line: line.split(')'), file.read().split('\n')))
+        self.scores = dict({'COM': 0})
+
+    def get_orbital_score(self):
+        score = 0
+        while len(self.orbits) > 0:
+            if self.orbits[0][0] in self.scores:
+                score += self.scores[self.orbits[0][0]] + 1
+                self.scores[self.orbits[0][1]] = self.scores[self.orbits[0][0]] + 1
+                self.orbits = self.orbits[1:]
+            self.orbits = self.orbits[1:] + self.orbits[:1]
+        return score
+
+
+print(OrbitalTree('input.txt').get_orbital_score())
