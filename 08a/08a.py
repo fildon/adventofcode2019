6ca9c7f5e38fe6b45e61d3bcbed3e2ebe6f50fd3
Rupert McKay <maxfmckay@gmail.com>
2019-12-08 08:09:19 +0000

    08a SOLVED

--- 08a/08a.py
+++ 08a/08a.py
@@ -1,2 +1,23 @@
 file = open("input.txt", "r")
-file_contents = file.read().strip()
+
+pixels = file.read(25 * 6).strip()
+leastzeroes = None
+checksum = None
+while len(pixels) > 0:
+    zeroes = 0
+    ones = 0
+    twos = 0
+    for pixel in pixels:
+        if pixel == "0":
+            zeroes += 1
+        if pixel == "1":
+            ones += 1
+        if pixel == "2":
+            twos += 1
+    if leastzeroes is None or zeroes < leastzeroes:
+        leastzeroes = zeroes
+        checksum = ones * twos
+    pixels = file.read(25 * 6).strip()
+
+print(checksum)
+# Solved 2176
